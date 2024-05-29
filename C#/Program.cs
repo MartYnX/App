cb8d78a17bf2d075b5cdd61f9cb74e4a316bacf1
MartYnX <martynx75@gmail.com>
2024-05-29 15:46:59 +0200

    C# finis

---
+++ C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json");
+
+var config = builder.Build();
+var connectionString = config.GetConnectionString("DefaultConnection");
+
+using (var conn = new MySqlConnection(connectionString))
+{
+    conn.Open();
+    using (var cmd = new MySqlCommand("SELECT * FROM succursale", conn))
+    {
+        using (var reader = cmd.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader.GetString(0)} {reader.GetString(1)}");
+            }
+        }
+    }
+}
